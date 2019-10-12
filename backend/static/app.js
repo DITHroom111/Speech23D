1fae8f2b5b82b1cc685b1066c45e543006dec545
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-10-12 19:04:57 +0200

    style

--- backend/static/app.js
+++ backend/static/app.js
@@ -87,35 +87,35 @@ function processFirstAsset(objectName, processAsset) {
 
 
 function drawAssetOnScene(asset, scene) {
-    var format = asset.formats.find( format => { return format.formatType === 'OBJ'; } );
+    var format = asset.formats.find(format => {return format.formatType === 'OBJ';});
 
     if ( format !== undefined ) {
         var obj = format.root;
-        var mtl = format.resources.find( resource => { return resource.url.endsWith( 'mtl' ) } );
+        var mtl = format.resources.find(resource => {return resource.url.endsWith('mtl')});
 
-        var path = obj.url.slice( 0, obj.url.indexOf( obj.relativePath ) );
+        var path = obj.url.slice(0, obj.url.indexOf(obj.relativePath));
 
         var loader = new THREE.MTLLoader();
-        loader.setCrossOrigin( true );
-        loader.setMaterialOptions( { ignoreZeroRGBs: true } );
-        loader.setTexturePath( path );
-        loader.load( mtl.url, function ( materials ) {
+        loader.setCrossOrigin(true);
+        loader.setMaterialOptions({ignoreZeroRGBs: true});
+        loader.setTexturePath(path);
+        loader.load(mtl.url, function(materials) {
             var loader = new THREE.OBJLoader();
-            loader.setMaterials( materials );
-            loader.load( obj.url, function ( object ) {
+            loader.setMaterials(materials);
+            loader.load(obj.url, function(object) {
                 var box = new THREE.Box3();
-                box.setFromObject( object );
+                box.setFromObject(object);
 
                 // re-center
                 var center = box.getCenter();
                 center.y = box.min.y;
-                object.position.sub( center );
+                object.position.sub(center);
 
                 // scale
                 var scaler = new THREE.Group();
-                scaler.add( object );
-                scaler.scale.setScalar( 6 / box.getSize().length() );
-                scene.add( scaler );
+                scaler.add(object);
+                scaler.scale.setScalar(6 / box.getSize().length());
+                scene.add(scaler);
             });
         });
     }
