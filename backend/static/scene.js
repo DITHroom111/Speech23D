64108d237d65dc5c02cf4f635468dd73d6b98d3e
Pavel Koshkin <koshkin.pasha@gmail.com>
2019-10-12 16:38:57 +0200

    create command

--- backend/static/scene.js
+++ backend/static/scene.js
@@ -1,15 +1,37 @@
-function processCommand(json) {
+function processCommand(json, scene) {
     if (typeof json["commandName"] === 'undefined') {
         throw new Error("No command name found!");
     }
-
+    if (typeof json["objectName"] === 'undefined') {
+        throw new Error("No object name found!");
+    }
 
     switch(json["commandName"]) {
         case 'create':
-            alert("Create command);
+            alert('create');
+            create(json["objectName"])
+            break;
+        case 'rotate':
+            alert('rotate;);
+            break;
+        case 'move':
+            alert('move');
+            break;
+        case 'teleportate':
+            alert('teleportate');
+            break;
+        case 'makeBigger':
+            alert('makeBigger');
+            break;
+        case 'makeSmaller':
+            alert('makeSmaller');
             break;
         default:
             alert("Unknown command);
             break;
     }
+}
+
+function create(objectName, scene) {
+
 }
